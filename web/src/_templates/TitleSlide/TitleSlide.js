44a8788604486efee4e3bab9b1171942bfcf4c19
Jakub Porębski <kuba.porebski3@gmail.com>
2024-05-29 02:53:12 +0200

    feat: title slide variations  + new font

--- web/src/_templates/TitleSlide/TitleSlide.js
+++ web/src/_templates/TitleSlide/TitleSlide.js
@@ -1,16 +1,18 @@
 import styles from './TitleSlide.module.scss';
+import clsx from 'clsx';
 
 function TitleSlide({ headline, subHeadlines, options }) {
+    const areSubheadlinesEmpty = subHeadlines.length === 0;
+
     return (
-        <div className={styles.container}>
+        <div className={clsx(styles.container, areSubheadlinesEmpty ? styles.head : styles.sub)}>
             <div className={styles.headline}>
-                <h1 className={headline}>{headline}</h1>
+                <h1>{headline}</h1>
             </div>
-            <div className={styles.subheadlines}>
-                {subHeadlines.map((hline, index) => <h2 key={`st-headline-${index}`}>{hline}</h2>)}
-            </div>
+            {!areSubheadlinesEmpty ?? <div className={styles.subheadlines}>
+                {subHeadlines.map((sline, index) => <h2 key={`st-headline-${index}`}>{sline}</h2>)}
+            </div>}
         </div>
     )
 }
-
 export default TitleSlide
